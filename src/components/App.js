82454bd6366bbfda343b67ab5ae99fb69e363e51
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-03-27 09:50:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
+import React, { useState } from 'react';
+//import './App.css';
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [fields, setFields] = useState([]);
+  const [count, setCount] = useState(0);
 
-const App = () => {
+  const addField = () => {
+    setFields([...fields, { key: count }]);
+    setCount(count + 1);
+  };
+
+  const deleteField = (key) => {
+    setFields(fields.filter((field) => field.key !== key));
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className='App'>
+      {fields.length === 0 ? (
+        <p>No fields in form.</p>
+      ) : (
+        fields.map((field) => (
+          <div key={field.key}>
+            <input type='text' name={`field-${field.key}`} />
+            <button className='delete' onClick={() => deleteField(field.key)}>
+              Delete
+            </button>
+          </div>
+        ))
+      )}
+      <button className='add' onClick={addField}>
+        Add field
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
